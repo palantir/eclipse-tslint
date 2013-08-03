3d0d315a195398ee3e36c2d776a030a10721626b
Ashwin Ramaswamy <aramaswamy@palantir.com>
2013-08-03 16:33:00 -0400

    rename nature handler

--- com.palantir.tslint/src/com/palantir/tslint/ToggleNatureHandler.java
+++ com.palantir.tslint/src/com/palantir/tslint/ToggleNatureHandler.java
@@ -10,7 +10,7 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-public class AddRemoveProjectNatureHandler extends AbstractHandler {
+public class ToggleNatureHandler extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		// TODO Auto-generated method stub
